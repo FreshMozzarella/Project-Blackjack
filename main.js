f6eb0c22b38336e6b34bdeeb99a30304e81d04ae
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-08-23 12:04:53 -0400

    refactor code for easier readbility, begain spinning animation

--- main.js
+++ main.js
@@ -1,304 +1,231 @@
-/*----- constants -----*/
-const symbols = ['🍒', '🍇', 'ッ', '💎', '𝟕']
-const slots = []
-const symbolPayout = {
-  '🍒': 100,
-  '🍇': 250,
-  'ッ': 500,
-  '💎': 1000,
-  '𝟕': 2000
-}
+/*----- app's state (variables) -----*/
+let slot1, slot2, slot3, slot4, slot5, slot6, slot7, slot8, slot9;
+const betAmounts = [1, 5, 20, 40];
+let betIndex = 0;
+let bet = 1;
+let coins = 100;
+let isSpinning = false;
+const MAX_BET = 10;
+const MIN_BET = 1;
 const AUDIO = new Audio('assets/other/SSBM.mp3')
-/*----- state variables -----*/
-let initialMoney = 500;
-let betIncrement = 0;
-let currentMoney;
-isPlaying = false;
- 
-/*----- cached elements  -----*/
+AUDIO.volume = 0.2;
+/*----- cached element references -----*/
+const slotEls = Array.from(document.querySelectorAll('.slot')); 
+const betEl = document.querySelector(".spinBtn");
+const coinsEl = document.querySelector(".total-amount");
+const musicButton = document.querySelector(".music");
+const decreaseBetBtn = document.querySelector(".bet-decrease");
+const increaseBetBtn = document.querySelector(".bet-increase");
+const spinBtn = document.querySelector(".spinBtn");
 
-const increaseBtn = document.querySelector('.bet-increase')
-const decreaseBtn = document.querySelector('.bet-decrease')
-const totalMoney = document.querySelector('.total-amount')
-const firstSlot = document.querySelector('.slot1')
-const secondSlot = document.querySelector('.slot2')
-const thirdSlot = document.querySelector('.slot3')
-const fourthSlot = document.querySelector('.slot4')
-const fifthSlot = document.querySelector('.slot5')
-const sixthSlot = document.querySelector('.slot6')
-const seventhSlot = document.querySelector('.slot7')
-const eighthSlot = document.querySelector('.slot8')
-const ninthSlot = document.querySelector('.slot9')
-const musicBtn = document.querySelector('.music')
-const spinBtn = document.querySelector('.spinBtn')
-const textWindow = document.querySelector('.alert')
-const slotWindow = document.querySelector('.slot-container')
-const cancelBtn = document.querySelector('.btn-cancel');
-const submitBtn = document.querySelector('.btn-submit');
-const modal = document.querySelector('.modal')
-const overlay = document.querySelector('.overlay')
-const modalTextBox = document.querySelector('.textbox')
-const slotDivEls = document.querySelectorAll('.slot-container > div')
 /*----- event listeners -----*/
-document.querySelector('.btn-close').addEventListener('click', closeModal)
-cancelBtn.addEventListener('click', closeModal)
-submitBtn.addEventListener('click', () => {
-  init();
-  closeModal();
-})
-increaseBtn.addEventListener('click', () => {
+decreaseBetBtn.addEventListener("click", decreaseBet);
+increaseBetBtn.addEventListener("click", increaseBet);
+spinBtn.addEventListener("click", spin);
+spinBtn.addEventListener("click", function() {
+  let slots = document.querySelectorAll(".slot");
+  slots.forEach(slot => slot.classList.add("spin"));
 
-  if (betIncrement === 0) {
-    if (currentMoney < 5) {
-      textWindow.innerText = "You don't have enough money"
-      return;
-    } else
-      currentMoney -= 5
-    totalMoney.innerText = `${currentMoney}`;
-    textWindow.innerText = 'you bet 5$ - all horizontal paylines';
-    betIncrement++;
-    decreaseBtn.removeAttribute('disabled');
-  }
-  else if (betIncrement === 1) {
-    if (currentMoney < 20) {
-      textWindow.innerText = "You don't have enough money"
-      return;
-    } else
-      currentMoney -= 20
-    totalMoney.innerText = `${currentMoney}`;
-    textWindow.innerText = 'you bet 20$ - all horizontal & vertical paylines';
-    betIncrement++
-  }
-  else if (betIncrement === 2) {
-    if (currentMoney < 40) {
-      textWindow.innerText = "You don't have enough money"
-      return;
-    } else
-      currentMoney -= 40
-    totalMoney.innerText = `${currentMoney}`;
-    textWindow.innerText = 'you bet 40$ Max Bet! - all horizontal, vertical, diagonal paylines';
-    betIncrement++;
-    increaseBtn.setAttribute('disabled', '');
-  }
-  else return 'CRITICAL ERROR'
-})
-decreaseBtn.addEventListener('click', () => {
-  if (betIncrement === 3) {
-    currentMoney += 40
-    totalMoney.innerText = `${currentMoney}`
-    textWindow.innerText = 'decreasing bet';
-    betIncrement--;
-    increaseBtn.removeAttribute('disabled');
-  }
-  else if (betIncrement === 2) {
-    currentMoney += 20
-    totalMoney.innerText = `${currentMoney}`
-    textWindow.innerText = 'decreasing bet';
-    betIncrement--
-  }
-  else if (betIncrement === 1) {
-    currentMoney += 5
-    totalMoney.innerText = `${currentMoney}`
-    betIncrement--;
-    textWindow.innerText = "Can't go any lower"
-    decreaseBtn.setAttribute('disabled', '')
-  } else return 'CRITICAL ERROR'
-})
-spinBtn.addEventListener('click', render)
-musicBtn.addEventListener('click', function togglePlay() {
-  AUDIO.volume = 0.01
-  isPlaying ? AUDIO.pause() : AUDIO.play()
-})
-AUDIO.onplaying = function () {
-  isPlaying = true
-};
-AUDIO.onpause = function () {
-  isPlaying = false;
-}
-
-
+  
+  setTimeout(() => {
+      slots.forEach(slot => slot.classList.remove("spin"));
+  }, 5000);  
+});
+musicButton.addEventListener("click", toggleAudioPlayback);
 /*----- functions -----*/
-function evalSlots() {
-  if (betIncrement === 0) {
-    console.log('checking bet increment and comparing to tests')
-    console.log(betIncrement)
-    const result = basicHorizontalTest();
-    if (result !== null){
-      return result;
-    }
-  } else if (betIncrement === 1) {
-    console.log('checking bet increment and comparing to all horizontal tests')
-    console.log(betIncrement)
-    const result1 = basicHorizontalTest();
-    const result2 = allHorizontalTest();
-    if (result1 !== null) {
-      return result1;
-    }
-    if (result2 !== null) {
-      return result2;
-    }
-
-  } else if (betIncrement === 2) {
-    console.log('checking for all vertical, horizontal, and basic tests')
-    console.log(betIncrement)
-    const result1 = basicHorizontalTest();
-    const result2 = allHorizontalTest();
-    const result3 = verticalTest();
-    if (result1 !== null) {
-      return result1;
-    }
-    if (result2 !== null) {
-      return result2;
-    }
-    if (result3 !== null) {
-      return result3;
-    }
-  } else if (betIncrement === 3) {
-    console.log('performing all available tests')
-    console.log(betIncrement)
-    const result1 = basicHorizontalTest();
-    const result2 = allHorizontalTest();
-    const result3 = verticalTest();
-    const result4 = diagonalTest();
-    if (result1 !== null) {
-      return result1;
-    }
-    if (result2 !== null) {
-      return result2;
-    }
-    if (result3 !== null) {
-      return result3;
-    }
-    if (result4 !== null) {
-      return result4;
-    }}
-};
-function spinning(){
-  slotDivEls.classList.add('hidden')
-  slotDivEls.setAttribute('id','isSpinning')
-  setTimeout(() =>{
-    slotDivEls.classList.remove('hidden')
-    slotDivEls.removeAttribute('id')
-  },4000)
-}
-
 function init() {
-  console.log('initializing')
-  currentMoney = initialMoney;
-  totalMoney.innerText = `${currentMoney}`
-  betIncrement = 0;
+  slot1 = slot2 = slot3 = "🍒";
+  slot4 = slot5 = slot6 = "🍒";
+  slot7 = slot8 = slot9 = "🍒";
+    render();
+    updateDisplay();
 }
-function render() {
-  console.log('rendering data')
-  spinBtn.setAttribute('disabled', '')
-  make2D();
-  renderSlots();
-  const output = evalSlots();
-  if (output !== null) {
-    renderPayout(output);
-  } else {
-    textWindow.innerText = 'no luck!';
-  }
-  renderGameOver();
-  decreaseBtn.setAttribute('disabled', '')
-  increaseBtn.removeAttribute('disabled')
-  slots.length = 0;
-  betIncrement = 0;
-  spinBtn.removeAttribute('disabled')
+function updateDisplay() {
+
+  betEl.innerText = "Spin = " + bet + "$";
+  coinsEl.innerText = coins;
+  spinBtn.disabled = coins < bet;
+}
+function decreaseBet() {
+  if (betIndex === 0) return;
+  betIndex--;
+  bet = betAmounts[betIndex];
+
+ 
+  updateBetButtonStates();
+
+  render();
 }
 
-function renderGameOver() {
-  if (currentMoney <= 5) {
-    console.log('opening modal')
-    openModal();
-  }
+function increaseBet() {
+  if (betIndex === betAmounts.length - 1) return;
+  betIndex++;
+  bet = betAmounts[betIndex];
+
+ 
+  updateBetButtonStates();
+
+  render();
 }
+
+function spin() {
+  
+  if (isSpinning || spinBtn.disabled) return; 
+
+  if (coins < bet) {
+      alert("Not enough coins to spin!");
+      return;
+  }
+
+  // Mark the start of the spinning
+  isSpinning = true;
+  spinBtn.disabled = true;
+
+  setTimeout(() => {
+      // Determine the random values for the slots
+      slot1 = getRandomValue();
+      slot4 = getRandomValue();
+      slot7 = getRandomValue();
+      slot2 = getRandomValue();
+      slot5 = getRandomValue();
+      slot8 = getRandomValue();
+      slot3 = getRandomValue();
+      slot6 = getRandomValue();
+      slot9 = getRandomValue();
+  
+
+      // Deduct the bet amount from the player's coins
+      coins -= bet;
+
+      // Evaluate the slots to determine any winnings and apply the payout
+      const winnings = evalSlots();
+      winnings.forEach(symbol => {
+        renderPayout(symbol);
+      });
+
+      // Mark the end of spinning
+      isSpinning = false;
+      spinBtn.disabled = coins < bet || (coins === 0);
+      // Refresh the display to show the results
+      render();
+  }, 2000);
+}
+
+function render() {
+    
+    slotEls[0].textContent = slot1;
+    slotEls[1].textContent = slot4;
+    slotEls[2].textContent = slot7;
+    slotEls[3].textContent = slot2;
+    slotEls[4].textContent = slot5;
+    slotEls[5].textContent = slot8;
+    slotEls[6].textContent = slot3;
+    slotEls[7].textContent = slot6;
+    slotEls[8].textContent = slot9;
+    updateDisplay();
+    const output = evalSlots();
+    
+}
+function updateBetButtonStates() {
+  decreaseBetBtn.classList.toggle("hidden", betIndex === 0);
+  increaseBetBtn.classList.toggle("hidden", betIndex === betAmounts.length - 1);
+}
+function evalSlots() {
+  let results = [];
+  switch (bet) {
+    case 1: 
+      results = basicHorizontalTest();
+      break;
+    case 5:
+      results = allHorizontalTest();
+      break;
+    case 20:
+      results = results.concat(verticalTest());
+      break;
+    case 40:
+      results = results.concat(diagonalTest());
+      break;
+  }
+  return results;
+}
+
 
 function basicHorizontalTest() {
-  if (
-    slots[1][0] === slots[1][1] &&
-    slots[1][1] === slots[1][2]
-  ) {
-    return slots[1][0];
-  }
-return null
+  if (slot4 === slot5 && slot5 === slot6) return [slot4];
+  return []
 }
 
 function allHorizontalTest() {
-  for (let i = 0; i < 3; i++) {
-    if (
-      slots[i][0] === slots[i][1] &&
-      slots[i][1] === slots[i][2]
-    ) {
-      return slots[i][0]; 
-    }
-  }
-  return null
+  let wins = [];
+  if (slot1 === slot2 && slot2 === slot3) wins.push(slot1);
+  if (slot4 === slot5 && slot5 === slot6) wins.push(slot4);
+  if (slot7 === slot8 && slot8 === slot9) wins.push(slot7);
+  return wins;
 }
+
 function verticalTest() {
-  for (let i = 0; i < 3; i++) {
-    if (
-      slots[0][i] === slots[1][i] &&
-      slots[1][i] === slots[2][i]
-    ) {
-      return slots[0][i]; 
-    }
-  }
-  return null
+  let wins = [];
+  if (slot1 === slot4 && slot4 === slot7) wins.push(slot1);
+  if (slot2 === slot5 && slot5 === slot8) wins.push(slot2);
+  if (slot3 === slot6 && slot6 === slot9) wins.push(slot3);
+  return wins;
 }
+
 function diagonalTest() {
-  if (slots[0][0] === slots[1][1] && slots[0][0] === slots[2][2]) {
-    return slots[0][0]; 
-  } else if (
-    slots[0][2] === slots[1][1] &&
-    slots[0][2] === slots[2][0]
-  ) {
-    return slots[0][2]; 
-  } else
-    return null; 
+  let wins = [];
+  if (slot1 === slot5 && slot5 === slot9) wins.push(slot1);
+  if (slot3 === slot5 && slot5 === slot7) wins.push(slot3);
+  return wins;
 }
 
-function closeModal() {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-}
-function openModal() {
-  if (currentMoney >= 2000) {
-    modalTextBox.innerText = "You are rich! Go home!"
-  } else {
-    modalTextBox.innerText = 'You ran out of money. Game over!'
+function renderPayout(symbol) {
+  if (symbol) {
+    switch (symbol) {
+      case '🍒':
+          coins += bet * 2;
+          break;
+      case '🍋':
+          coins += bet * 3;
+          break;
+      case '🍊':
+          coins += bet * 4;
+          break;
+      case '🍉':
+          coins += bet * 5;
+          break;
+      case '🍇':
+          coins += bet * 6;
+          break;
+      case '🍓':
+          coins += bet * 7;
+          break;
+      case '🍍':
+          coins += bet * 8;
+          break;
+      case '🥝':
+          coins += bet * 9;
+          break;
+      case '💎':
+          coins += bet * 10;
+          break;
+      case '7️⃣':
+          coins += bet * 15;
+          break;
+    } 
+    updateDisplay();
   }
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
 }
 
-function make2D() {
-  currentMoney -= 5
-  totalMoney.innerText = `${currentMoney}`;// betIncrement - 0$ gets charged -5$ for spinning the basic reel, 1- 5$-5$, 2- 5$-$20, 3- 5$- 40$
-  textWindow.innerText = 'Spinning!'
-  for (i = 0; i < 3; i++) {
-    slots[i] = [];
-    for (j = 0; j < 3; j++) {
-      slots[i][j] = symbols[Math.floor(Math.random() * symbols.length)]
-    }
-  }
-  return slots
-}
-function renderSlots() {
-  slots.forEach((row, rowIndex) => {
-    row.forEach((symbol, colIndex) => {
-      const index = rowIndex * 3 + colIndex;
-      slotDivEls[index].innerText = symbol;
-    });
-  });
-}
 
-function renderPayout(output) {
-  if (output in symbolPayout) {
-    currentMoney += symbolPayout[output];
-    totalMoney.innerText = `${currentMoney}`;
+function getRandomValue() {
+    const symbols = ['🍒', '🍋', '🍊', '🍉', '🍇', '🍓', '🍍', '🥝', '💎', '7️⃣'];
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+function toggleAudioPlayback() {
+  if (AUDIO.paused) {
+      AUDIO.play();
   } else {
-    textWindow.innerText = 'no luck!'
+      AUDIO.pause();    
   }
 }
 
